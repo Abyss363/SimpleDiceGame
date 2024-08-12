263a9d72188fee4703567586bc4f85bf4a269aa3
Abyss363 <fortuneodo@gmail.com>
2024-08-12 13:57:45 +0100

    Completed

--- Program.cs
+++ Program.cs
@@ -5,24 +5,33 @@ int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
-
-if (roll1 == roll2 || roll2 == roll3 || roll3 == roll1)
-{ 
-    Console.Write("You rolled doubles!! Plus 2 to score!!");
-    total += 2;
-}
+Console.WriteLine($"Dice Roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if (roll1 == roll2 && roll2 == roll3)
 {
-    Console.WriteLine("YOU ROLLED TRIPLES!!! +SIX TO SCORE!!!")
+    Console.WriteLine("YOU ROLLED TRIPLES!!! +SIX TO SCORE!!!");
+    total += 6;
+}
+else if (roll1 == roll2 || roll2 == roll3 || roll3 == roll1)
+{
+    Console.WriteLine("You rolled doubles!! Plus 2 to score!!");
+    total += 2;
 }
 
-if (total >= 15)
-{
-    Console.WriteLine("You Win!!(^o^)");
-}
 
-if (total < 15)
+if (total >= 16)
 {
-    Console.WriteLine("You Lose >_<!!");
+    Console.WriteLine("You win a new car!!(^o^)");
+}
+else if(total >= 10)
+{
+    Console.WriteLine("You win a new laptop!!");
+}
+else if(total == 7)
+{
+    Console.WriteLine("You win a free trip");
+}
+else
+{
+    Console.WriteLine("You win a kitten");
 }
